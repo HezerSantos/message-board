23678b5b418ef3d5a097d3867e3d058f74fb051f
HezerSantos <HezerSantos24@gmail.com>
2025-03-28 20:50:50 -0500

    Lol

--- validation/messageValidator.js
+++ validation/messageValidator.js
@@ -2,9 +2,10 @@ const { body, validationResult } = require("express-validator");
 
 exports.validateMessage =[
     body("message")
+        .isLength({min: 10}).withMessage("Message must be at least 10 characters")
         .notEmpty().withMessage("Message Cannot be empty")
         .trim()
         .escape()
 ]
 
-//.isLength({min: 10}).withMessage("Message must be at least 10 characters")
+//
