ff9b471008c94b2a0815d378c743aca44f2fe7e3
HezerSantos <HezerSantos24@gmail.com>
2025-03-28 21:39:10 -0500

    herm

--- validation/messageValidator.js
+++ validation/messageValidator.js
@@ -2,9 +2,9 @@ const { body, validationResult } = require("express-validator");
 
 exports.validateMessage =[
     body("message")
+        .trim()
         .isLength({min: 10}).withMessage("Message must be at least 10 characters")
         .notEmpty().withMessage("Message Cannot be empty")
-        .trim()
         .escape()
 ]
 
